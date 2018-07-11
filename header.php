51f2e1333c84f2ef8625d15d88f54c00f539a807
James Pederson <james@jpederson.com>
2018-07-11 06:33:22 -0500

    Bump CSS and JS versions to force cache break.

--- header.php
+++ header.php
@@ -16,7 +16,7 @@
 <![endif]-->
 
 <?php wp_head(); ?>
-<link href="<?php bloginfo( "template_url" ) ?>/css/main.css?v=8" rel="stylesheet" type="text/css">
+<link href="<?php bloginfo( "template_url" ) ?>/css/main.css?v=9" rel="stylesheet" type="text/css">
 
 </head>
 <body <?php body_class(); ?>>
