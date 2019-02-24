332c2e4f21f999265faf2f2a17aaa4ff3b00e1d9
James Pederson <james@jpederson.com>
2019-02-24 15:10:58 -0600

    Change comments to VA Methodist title.

--- single.php
+++ single.php
@@ -28,7 +28,7 @@ get_header();
 
 				(function() { // DON'T EDIT BELOW THIS LINE
 				var d = document, s = d.createElement('script');
-				s.src = '//st-matthews-fcu.disqus.com/embed.js';
+				s.src = 'https://va-united-methodist-credit-union.disqus.com/embed.js';
 				s.setAttribute('data-timestamp', +new Date());
 				(d.head || d.body).appendChild(s);
 				})();
