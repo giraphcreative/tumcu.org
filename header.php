d28a4bc8321c43782559becedda2f035f3306048
James Pederson <james@jpederson.com>
2019-07-16 10:50:56 -0500

    Update the title bar text.

--- header.php
+++ header.php
@@ -6,7 +6,7 @@
 <meta charset="<?php bloginfo( 'charset' ); ?>" />
 <meta name="viewport" content="width=device-width,initial-scale=1" />
 
-<title><?php wp_title( '|', true, 'right' ); ?> VA United Methodist CU</title>
+<title><?php wp_title( '|', true, 'right' ); ?> TUMCU</title>
 
 <link rel="profile" href="http://gmpg.org/xfn/11" />
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
