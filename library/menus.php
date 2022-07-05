0609e196bbdb10421cf6ce6c7c4aaf2e0604ccb0
James Pederson <james@jpederson.com>
2022-07-05 12:07:01 -0500

    Update menu names to match their position. Update dependencies.

--- library/menus.php
+++ library/menus.php
@@ -4,11 +4,11 @@
 // register a couple nav menus
 register_nav_menus( array(
 	'main-menu' => 'Main',
-	'footer-one' => 'Footer - About',
-	'footer-two' => 'Footer - Loans',
-	'footer-three' => 'Footer - Save',
-	'footer-four' => 'Footer - Pastors',
-	'footer-five' => 'Footer - Churches',
+	'footer-one' => 'Footer - One',
+	'footer-two' => 'Footer - Two',
+	'footer-three' => 'Footer - Three',
+	'footer-four' => 'Footer - Four',
+	'footer-five' => 'Footer - Five',
 	'footer-notices' => 'Footer - Notices'
 ) );
 
